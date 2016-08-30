91881f50c2b83db61ef504b3302326af93872eb3
Van Chan Ngo <chan.ngo2203@gmail.com>
2016-08-30 19:14:17 -0400

    update stack

--- src/stack.v
+++ src/stack.v
@@ -5,3 +5,12 @@
  *====================================================================*)
 
 
+Require Import mathcomp.ssreflect.ssreflect.
+From mathcomp Require Import ssrfun ssrbool eqtype ssrnat seq fintype finfun.
+
+Require Import bitsrep bitsops .
+
+Set Implicit Arguments.
+Unset Strict Implicit.
+Import Prenex Implicits.
+
