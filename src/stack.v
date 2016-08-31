afbe3e47dacb345d074c9e3aeb690034853a4f15
Van Chan Ngo <chan.ngo2203@gmail.com>
2016-08-31 17:00:03 -0400

    update stack

--- src/stack.v
+++ src/stack.v
@@ -20,3 +20,44 @@ Import Prenex Implicits.
 (*=Stack *)
 Definition Stack := list EVMWORD.
 
+(* Initilize the stack, empty stack *)
+Definition initialStack : Stack := nil.
+
+(* Stack size *)
+Definition maxSize := 1024.
+
+(* Check the stack is empty *)
+Definition isEmpty (s : Stack) : bool :=
+  match s with
+    | nil => true
+    | _ => false
+  end.
+
+(* Return a singleton stack *)
+Definition  singletonStack (evmw : EVMWORD) : Stack :=
+  evmw::nil.
+
+(* Peek the head element *)
+Definition peek (s : Stack) : option EVMWORD :=
+  match s with
+    | nil => None
+    | w::_ => Some w
+  end.
+
+(* Peek the head element if [s] is not nil, otherwise return [dw] *)
+Definition peekdefault (s : Stack) (dw : EVMWORD) : EVMWORD :=
+  match s with
+    | nil => dw
+    | w::_ => w
+  end.
+
+(*---------------------------------------------------------------------
+  Push BYTE, WORD, DWORD, QWORD, DQWORD, ADDRESS, and EVMWORD.
+  
+  [pushARRAY] pushes an abitrary number of bytes. 
+ ---------------------------------------------------------------------*)
+
+Definition pushEVMWORD (s : Stack) (evmw : EVMWORD) : option Stack :=
+  if (length s) < maxSize then
+    Some (evmw::s)
+  else None.
