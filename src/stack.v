80c7d7e35997e8435905e38201b6ed5eea8ade24
Van Chan Ngo <chan.ngo2203@gmail.com>
2016-09-01 14:08:15 -0400

    update stack

--- src/stack.v
+++ src/stack.v
@@ -6,11 +6,10 @@
 
 
 Require Import mathcomp.ssreflect.ssreflect.
-From mathcomp Require Import ssrfun ssrbool eqtype ssrnat seq fintype finfun.
+From mathcomp Require Import ssrfun ssrbool eqtype ssrnat seq fintype tuple zmodp.
 
 Require Import bitsrep bitsops.
 
-Require Import listhelp.
 
 Set Implicit Arguments.
 Unset Strict Implicit.
@@ -33,6 +32,11 @@ Definition isEmpty (s : Stack) : bool :=
     | _ => false
   end.
 
+(* Check the stack is full *)
+Definition isFull (s : Stack) : bool :=
+  if (length s) >= maxSize then true
+  else false.
+
 (* Return a singleton stack *)
 Definition  singletonStack (evmw : EVMWORD) : Stack :=
   evmw::nil.
@@ -53,22 +57,61 @@ Definition peekdefault (s : Stack) (dw : EVMWORD) : EVMWORD :=
 
 (*---------------------------------------------------------------------
   Push BYTE, WORD, DWORD, QWORD, DQWORD, ADDRESS, and EVMWORD.
-  
-  [pushARRAY] pushes an abitrary number of bytes. 
+ 
  ---------------------------------------------------------------------*)
 
-Definition pushBYTE (s : Stack) (b : BYTE) : option Stack :=
+Definition pushBYTE (s : Stack) (b : BYTE) :  Stack :=
   (* Zero extension of [b] to EVMWORD size *)
-  if (length s) < maxSize then
-    Some ((BYTEtoEVMWORD b)::s)
-  else None.
+  (BYTEtoEVMWORD b)::s.
 
-Definition pushWORD (s : Stack) (w : WORD) : option Stack :=
-  if (length s) < maxSize then
-    Some ((WORDtoEVMWORD w)::s)
-  else None.
+Definition pushWORD (s : Stack) (w : WORD) : Stack :=
+  (WORDtoEVMWORD w)::s.
 
-Definition pushEVMWORD (s : Stack) (evmw : EVMWORD) : option Stack :=
-  if (length s) < maxSize then
-    Some (evmw::s)
-  else None.
+Definition pushDWORD (s : Stack) (w : DWORD) : Stack :=
+  (DWORDtoEVMWORD w)::s.
+
+Definition pushQWORD (s : Stack) (qw : QWORD) : Stack :=
+  (QWORDtoEVMWORD qw)::s.
+
+Definition pushDQWORD (s : Stack) (dqw : DQWORD) : Stack :=
+  (DQWORDtoEVMWORD dqw)::s.
+
+Definition pushADDRESS (s : Stack) (addr : ADDRESS) : Stack :=
+  (ADDRESStoEVMWORD addr)::s.
+
+Definition pushEVMWORD (s : Stack) (evmw : EVMWORD) : Stack :=
+  evmw::s.             
+
+
+(*--------------------------------------------------------------------
+ Pop EVMWORD
+ --------------------------------------------------------------------*)
+Definition popEVMWORD (s : Stack) : Stack :=
+  match s with
+    | nil => nil
+    | w::t => t
+  end.
+
+
+Definition apply_top (f : EVMWORD -> EVMWORD) (s : Stack) : Stack :=
+  match s with
+    | nil => nil
+    | w::t => (f w)::t
+  end.
+
+
+Definition replace_top (w : EVMWORD) (s : Stack) : Stack :=
+  match s with
+    | nil => nil
+    | h::t => w::t
+  end.
+
+(* Truncate [s] into size [n] *)
+Fixpoint truncate (n : nat) (s : Stack) : Stack :=
+  if n < (length s) then
+        match s with
+          | _::t => truncate n t
+          | _ => nil
+        end
+  else s.
+
