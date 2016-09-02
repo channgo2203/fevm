a05acc630be36d2df6c6b06e82c96004a917f8ac
Van Chan Ngo <chan.ngo2203@gmail.com>
2016-09-02 17:21:30 -0400

    add instrsyntax

---
+++ src/instrsyntax.v
@@ -0,0 +1,14 @@
+(*=============================================================================
+ Instruction syntax and semantics
+ ============================================================================*)
+
+
+Require Import mathcomp.ssreflect.ssreflect.
+From mathcomp Require Import ssrfun ssrbool eqtype ssrnat seq fintype tuple zmodp.
+
+Require Import bitsrep bitsprops bitsops bitsopsprops mem storage stack instr.
+
+Set Implicit Arguments.
+Unset Strict Implicit.
+Import Prenex Implicits.
+
