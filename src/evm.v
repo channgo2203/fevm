a7d671de603aad636da2cf8205942fcca0b0726e
Van Chan Ngo <chan.ngo2203@gmail.com>
2016-09-06 17:03:46 -0400

    add evm.v

---
+++ src/evm.v
@@ -0,0 +1,30 @@
+(*=============================================
+ The Ethereum Virtual Machine
+ =============================================*)
+
+
+Require Import mathcomp.ssreflect.ssreflect.
+From mathcomp Require Import ssrfun ssrbool eqtype ssrnat seq fintype finfun tuple zmodp.
+
+Require Import bitsrep bitsops cursor pmap reader writer mem stack instr program state ere.
+
+Set Implicit Arguments.
+Unset Strict Implicit.
+Import Prenex Implicits.
+
+(*= EVM Exception *)
+Inductive EVMException :=
+| BreakPointHit
+| BadInstruction
+| BadJumpDestination
+| OutOfGas
+| OutOfStack
+| StackUnderflow.
+
+(*=EVM *)
+Record EVMachine :=
+  mkEVMachine {
+      m_ere : EREnvironment;
+      m_evmstate : EVMState
+    }.
+
